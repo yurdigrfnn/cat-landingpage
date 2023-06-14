dff37d8ee0abd1219cbec416abc26f09302f5102
yurdiansyah <yurdiansyah143@gmail.com>
2023-06-14 23:25:55 +0700

    feature: ✨Home

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,52 +6,53 @@ import logoImg from "../../assets/Logo.png";
 export const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <div className="mt-10">
-      <div className="md:flex justify-between content-center hidden">
-        <div className="lg:w-[20%] w-[15%] my-auto">
-          <img src={logoImg} alt="" className="w-[150px]" />
+    <div className="absolute w-full z-10">
+      <div className="mt-10">
+        <div className="lg:w-[88.61%] w-[95%] mx-auto md:flex justify-between content-center hidden">
+          <div className="lg:w-[20%] w-[15%] my-auto">
+            <img src={logoImg} alt="" className="w-[150px]" />
+          </div>
+          <ul className="flex lg:w-[60%] w-[70%] justify-between my-auto">
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Max For Dog
+            </li>
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Max For Cat
+            </li>
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Forum
+            </li>
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Pet Talk
+            </li>
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Max share
+            </li>
+            <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
+              Contact Us
+            </li>
+          </ul>
+          <div className="flex justify-end  lg:w-[20%] w-[15%]">
+            <button className="bg-[#299FFF] text-white px-8 py-[14px] text-sm font-bold rounded-[10px]">
+              Join Us
+            </button>
+          </div>
         </div>
-        <ul className="flex lg:w-[60%] w-[70%] justify-between my-auto">
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Max For Dog
-          </li>
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Max For Cat
-          </li>
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Forum
-          </li>
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Pet Talk
-          </li>
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Max share
-          </li>
-          <li className="text-base font-bold hover:text-[#299FFF] cursor-pointer">
-            Contact Us
-          </li>
-        </ul>
-        <div className="flex justify-end  lg:w-[20%] w-[15%]">
-          <button className="bg-[#299FFF] text-white px-8 py-[14px] text-sm font-bold rounded-[10px]">
-            Join Us
+        <div className="md:hidden relative">
+          <NavbarMobile
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+          <button
+            type="button"
+            className="flex h-10 w-10 ml-6 absolute justify-center rounded-full text-gray-500"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <MenuBarIcon className="h-6 w-6 text-black" aria-hidden="true" />
           </button>
-        </div>
-      </div>
-      <div className="md:hidden relative">
-        <NavbarMobile
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-        />
-        <button
-          type="button"
-          className="flex h-10 w-10 items-center absolute justify-center rounded-full text-gray-500"
-          onClick={() => setSidebarOpen(true)}
-        >
-          <span className=""></span>
-          <MenuBarIcon className="h-6 w-6 text-black" aria-hidden="true" />
-        </button>
-        <div className="text-center flex justify-center items-start content-center w-full">
-          <img src={logoImg} alt="" className="w-[150px]" />
+          <div className="text-center flex justify-center items-start content-center w-full">
+            <img src={logoImg} alt="" className="w-[150px]" />
+          </div>
         </div>
       </div>
     </div>
--- src/features/home/components/Home.jsx
+++ src/features/home/components/Home.jsx
@@ -1,9 +1,82 @@
 import { Navbar } from "../../../components/Navbar";
+import MLogo from "./../assets/m-logo.png";
+import Person from "./../assets/person.png";
+import Mlogo2 from "./../assets/m-logo-2.png";
+import Cat from "./../assets/cat.png";
+import Dog from "./../assets/dog.png";
 
 export const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col md:flex-row">
       <Navbar />
+      <div className="w-full relative">
+        <div className="md:absolute md:mb-0 -mb-5 top-0 right-0 left-0">
+          <div className="lg:w-[88.61%] w-[95%] mx-auto md:mt-[232px]">
+            <div className="md:max-w-[524px]">
+              <h1 className="md:text-[64px] md:text-start  text-3xl font-extrabold leading-9 text-center mt-[112px] md:leading-[75px]">
+                Everything Your{" "}
+                <span className="bg-[#F1CB80] rounded-lg px-2">Companion</span>{" "}
+                Needs the Most
+              </h1>
+              <p className="md:text-lg text-base md:text-start text-center font-medium text-[#231F20] mt-4">
+                Perfect thread & good care for perfect companion, is all you
+                gave for your four legged angel
+              </p>
+              <div className=" mt-10 w-full flex md:inline-block justify-center">
+                <button className="bg-[#299FFF] text-white px-8 py-[14px] text-sm font-bold rounded-[10px]">
+                  Get Free Sample
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" bg-[#F6F6F6] relative w-full z-[-1]">
+          <div className="flex justify-between">
+            <div className="w-[524px] h-[489px]">
+              <img
+                src={Mlogo2}
+                alt=""
+                className="mt-32 md:inline-block hidden "
+              />
+            </div>
+            <img src={MLogo} alt="" className="mt-32 ml-auto" />
+          </div>
+          <img src={Person} alt="" className="absolute bottom-0 right-0" />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            className="absolute bottom-0 -mb-1"
+          >
+            <path
+              fill="#ffffff"
+              fillOpacity="1"
+              d="M0,192L120,213.3C240,235,480,277,720,277.3C960,277,1200,235,1320,213.3L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+            ></path>
+          </svg>
+          <div className="z-10 w-full flex justify-center absolute bottom-0 -mb-8 md:-mb-4">
+            <div className="xl:w-7/12 w-[87.5%] md:w-9/12 flex justify-between">
+              <div className="w-[48.05%] rounded-3xl md:rounded-[36px] relative bg-[#B27FB9] flex text-end justify-end  py-[14px] md:py-[52px]">
+                <div className="sm:w-[110px] w-[90px] md:w-max absolute bottom-0  left-0">
+                  <img src={Dog} alt="" className="" />
+                </div>
+                <h1 className=" text-white md:text-[32px] text-[24px] text-2xl w-[158px] mr-[14px]  sm:mr-[20px]">
+                  I’m a Dog Person
+                </h1>
+              </div>
+              <div className="w-[48.05%] rounded-3xl md:rounded-[36px] relative bg-[#60AEEE] flex text-end justify-end py-[14px] md:py-[52px]">
+                <div className=" sm:w-[110px] w-[90px] md:w-max absolute bottom-0 left-0">
+                  <img src={Cat} alt="" className="" />
+                </div>
+                <h1 className=" text-white md:text-[32px] text-[24px] text-2xl w-[158px] mr-[14px] sm:[16px] sm:mr-[20px]">
+                  I’m a Cat Person
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
